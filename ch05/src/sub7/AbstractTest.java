fe8ff60b69921ca7d9be228670ed77c6205bada8
loveu9911111 <loveu9911111@gmail.com>
2024-07-22 17:07:43 +0900

    add ch10

---
+++ ch05/src/sub7/AbstractTest.java
@@ -0,0 +1,26 @@
+package sub7;
+/*
+ * 날짜 : 2024/07/10
+ * 이름 : 최준혁
+ * 내용 : 자바 추상클래스 연습하기
+ */
+public class AbstractTest {
+	public static void main(String[] args) {
+		
+		// 추상클래스는 객체 생성 x
+		// Animal animal = new Animal();
+		
+		Animal tiger = new Tiger();
+		Animal eagle = new Eagle();
+		Animal shark = new Shark();
+		
+		tiger.move();
+		tiger.hunt();
+		
+		eagle.move();
+		eagle.hunt();
+		
+		shark.move();
+		shark.hunt();
+	}
+}
