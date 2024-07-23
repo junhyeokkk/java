dc83f74c2cb872a18afe09c306faffa04d9dafca
loveu9911111 <loveu9911111@gmail.com>
2024-07-23 12:32:54 +0900

    add ch11

---
+++ ch11/src/sub4/StreamMapTest.java
@@ -0,0 +1,67 @@
+package sub4;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/*
+ * 날짜 : 2024/07/23	
+ * 이름 : 최준혁
+ * 내용 : 자바 컬렉션 스트림 맵 실습하기
+ */
+class Person {
+	private String name;
+	private int age;
+	
+	public Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", age=" + age + "]";
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+}
+
+public class StreamMapTest {
+	public static void main(String[] args) {
+
+		List<Person> people = new ArrayList<>();
+		people.add(new Person("김유신", 23));
+		people.add(new Person("김춘추", 21));
+		people.add(new Person("장보고", 33));
+		people.add(new Person("강감찬", 43));
+		people.add(new Person("이순신", 53));
+
+		// map 스트림
+		List<String> names = people.stream()
+								   .map((person)->{
+									   return person.getName(); //새로운 데이터 변환해서 다음 스트림으로 전달
+								   })
+								   .collect(Collectors.toList()); //스트림으로 전송된 데이터 집합체
+		System.out.println(names);
+		
+		// flatMap 스트림
+		List<List<String>> lists = List.of(
+											Arrays.asList("apple","banana"),
+											Arrays.asList("cherry","grape"),
+											Arrays.asList("orange","mango")
+										);
+		System.out.println(lists);
+		
+		List<String> fruits = lists.stream()
+											.flatMap((list)->{
+												return list.stream();
+											}).
+											collect(Collectors.toList());
+		System.out.println(fruits);
+		
+	}
+}
